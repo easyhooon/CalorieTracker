49a6173374d01efb5f52a3f467a6c2c421aec09a
easyhooon <mraz3068@gmail.com>
2022-11-09 13:18:05 +0900

    migrate gradle groovy to kotlin-dsl

---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,9 @@
+object Plugins {
+    const val ANDROID_APPLICATION = "com.android.application"
+    const val ANDROID_LIBRARY = "com.android.library"
+    const val KOTLIN_ANDROID = "org.jetbrains.kotlin.android"
+    const val DAGGER_HILT = "com.google.dagger.hilt.android"
+    const val HILT_PLUGIN = "dagger.hilt.android.plugin"
+    const val PARCELIZE = "kotlin-parcelize"
+    const val KAPT = "kotlin-kapt"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,6 @@
+object Versions {
+    // Project
+    const val AGP = "7.1.3"
+    const val KOTLIN = "1.6.10"
+    const val HILT = "2.42"
+}
