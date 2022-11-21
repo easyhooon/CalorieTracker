600d9709d0d8494bde18a63bdc52f01f4f4898f7
easyhooon <mraz3068@gmail.com>
2022-11-21 11:23:34 +0900

    feat: SearchViewModel 구성

---
+++ tracker/tracker_presentation/src/main/java/com/kenshi/tracker_presentation/search/SearchState.kt
@@ -0,0 +1,9 @@
+package com.kenshi.tracker_presentation.search
+
+
+data class SearchState  (
+    val query: String = "",
+    val isHintVisible: Boolean = false,
+    val isSearching: Boolean = false,
+    val trackableFood: List<TrackableFoodUiState> = emptyList()
+)
---
+++ tracker/tracker_presentation/src/main/java/com/kenshi/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.kenshi.tracker_presentation.search
+
+import com.kenshi.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
